cef567ee620ff507ce9f24063bfb325869ccc915
Liran Shoshana <liranshoshana93@gmail.com>
2021-02-18 10:24:40 +0200

    Added explosion animation to bullet

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject exp;
     public float speed = 20f;
     public Rigidbody2D rb;
     void Start()
     {
         rb.velocity = transform.up * speed;
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Arena")
         {
             Destroy(gameObject);
+            exp = (GameObject) Instantiate(exp, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(exp, 0.2f);
+
         }
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
             Destroy(collision.gameObject);
+            exp = (GameObject)Instantiate(exp, gameObject.transform.position, gameObject.transform.rotation);
             Destroy(gameObject);
-            Debug.Log("Enemy destroy");
+            Destroy(exp,0.2f);
         }
         if (collision.gameObject.tag == "Tank")
         {
             Destroy(collision.gameObject);
+            exp = (GameObject)Instantiate(exp, transform.position, transform.rotation);
             Destroy(gameObject);
+            Destroy(exp,0.2f);
         }
 
     }
